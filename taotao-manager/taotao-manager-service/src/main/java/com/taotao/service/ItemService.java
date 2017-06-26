903b120cf4594f59a29f9d23b7caf03d4dd2f596
yinxin <179323568@qq.com>
2017-06-26 22:51:49 +0800

    taotao 11

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+/**
+ * @author dell
+ *
+ */
+public interface ItemService {
+
+	TbItem getItemById(Long itemId);
+}
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,15 +9,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
+/**
+ * 商品管理Controller
+ * <p>Title: ItemController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.com</p> 
+ * @author	入云龙
+ * @date	2015年9月2日上午10:52:46
+ * @version 1.0
+ */
+
+@Controller
 public class ItemController {
-	
+
 	@Autowired
 	private ItemService itemService;
 	
 	@RequestMapping("/item/{itemId}")
 	@ResponseBody
-	private TbItem getItemById(@PathVariable Long itemId) {
-		TbItem item = itemService.getItemById(itemId);
-		return item;
+	public TbItem getItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
 	}
+	
+	
 }
